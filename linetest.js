7164051c7cbd0aebb29874411241908a89e6b0ea
Shejiawei <wei19970130@gmail.com>
2016-11-16 18:17:54 +0800

    617

--- linetest.js
+++ linetest.js
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <script src="https://d3js.org/d3.v4.min.js"></script>
-  <title>Document</title>
-</head>
-<body>
-<script type="text/javascript">
+
+function linetest()
+{
 var width = 1366;
 var height = 768;
 var ctrl=d3.select("body").append("svg").attr("width",width).attr("height",height);
-	d3.csv("./20151222.csv",function(data){
+	d3.csv("https://shejiawei.github.io/D3/20151222.csv",function(data){
 		var ln = data.length;
 		console.log(data);
- 	var maxy = 1080
+ 	var maxy = 1100
 	 var lines = d3.line().x(function(d,i){return i*(width/ln);
 	 	}).y(function(d){return height*(1-d.StnPres/maxy)});
 	 ctrl.append("path").data([data]).attr("d",lines).attr("stroke","red").attr("fill","none");
 	}
 	)
-</script>
-</body>
-</html>
+}
