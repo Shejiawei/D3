b52207aaf5ac5f2fa9425792e2bcf1a6d434f433
Shejiawei <wei19970130@gmail.com>
2016-11-16 18:25:54 +0800

    625

--- linetest.js
+++ linetest.js
@@ -1,13 +1,13 @@
 
 function linetest()
 {
-var width = 500;
+var width = 1000;
 var height = 500;
 var ctrl=d3.select("body").append("svg").attr("width",width).attr("height",height);
 	d3.csv("https://shejiawei.github.io/D3/20151222.csv",function(data){
 		var ln = data.length;
 		console.log(data);
- 	var maxy = 1030
+ 	var maxy = 1050
 	 var lines = d3.line().x(function(d,i){return i*(width/ln);
 	 	}).y(function(d){return height*(1-d.StnPres/maxy)});
 	 ctrl.append("path").data([data]).attr("d",lines).attr("stroke","red").attr("fill","none");
